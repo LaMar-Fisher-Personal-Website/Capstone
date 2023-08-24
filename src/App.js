1172cb900be3e88a2b2571991d14acf15f479373
LaMar Fisher <lsfisher503@gmail.com>
2023-08-23 17:31:30 -0700

    completed shopcart and fix product list bug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ProductList from './ProductList';
 import ShopCart from './ShopCart';
+import Checkout from './Checkout'; // Import the Checkout component
 
 function App() {
-    // State to manage the cart items
     const [cartItems, setCartItems] = useState([]);
+    const [showCheckout, setShowCheckout] = useState(false);
+
+    useEffect(() => {
+        const storedCart = localStorage.getItem('cart');
+        if (storedCart) {
+            setCartItems(JSON.parse(storedCart));
+        }
+    }, []); // Empty dependency array ensures this effect runs only once when the component mounts
+
+    // Function to add an item to the cart
+    const addToCart = (product) => {
+        // Check if the product is already in the cart
+        const existingItem = cartItems.find(item => item.id === product.id);
+
+        if (existingItem) {
+            // If the product is already in the cart, update its quantity
+            const updatedCart = cartItems.map(item =>
+                item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+            );
+            setCartItems(updatedCart);
+        } else {
+            // If the product is not in the cart, add it with quantity 1
+            setCartItems([...cartItems, { ...product, quantity: 1 }]);
+        }
+    };
 
     return (
         <div>
-            {/* Render other parts of your app */}
-            <ProductList cartItems={cartItems} setCartItems={setCartItems} />
-            <ShopCart cartItems={cartItems} />
+          <ProductList addToCart={addToCart} />
+            <ShopCart
+                cartItems={cartItems}
+                setCartItems={setCartItems}
+                addToCart={addToCart} // Pass the addToCart function down
+            />
+            {showCheckout ? (
+                <Checkout
+                    cartItems={cartItems}
+                    setCartItems={setCartItems}
+                    addToCart={addToCart} // Pass the addToCart function down
+                />
+            ) : null}
+            <button onClick={() => setShowCheckout(true)}>Proceed to Checkout</button>
         </div>
     );
 }
 
 export default App;
+
+
--- src/ShopCart.js
+++ src/ShopCart.js
@@ -9,7 +9,9 @@ function ShopCart({ cartItems, setCartItems }) {
             item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
         );
         setCartItems(updatedCart);
+        localStorage.setItem('cart', JSON.stringify(updatedCart));
     };
+    
 
     const handleDecrementQuantity = (itemId) => {
         // Find the item in the cart and decrement its quantity
@@ -17,13 +19,24 @@ function ShopCart({ cartItems, setCartItems }) {
             item.id === itemId && item.quantity > 1 ? { ...item, quantity: item.quantity - 1 } : item
         );
         setCartItems(updatedCart);
+        localStorage.setItem('cart', JSON.stringify(updatedCart));
     };
 
     const handleRemoveItem = (itemId) => {
         // Remove the item from the cart
         const updatedCart = cartItems.filter(item => item.id !== itemId);
+        localStorage.removeItem('cart');
         setCartItems(updatedCart);
     };;
+
+    const handleEditQuantity = (itemId, newQuantity) => {
+        const updatedCart = cartItems.map(item =>
+            item.id === itemId ? { ...item, quantity: newQuantity } : item
+        );
+        setCartItems(updatedCart);
+        localStorage.setItem('cart', JSON.stringify(updatedCart));
+    };
+    
 
       // Calculate total price
       const totalPrice = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
@@ -32,15 +45,21 @@ function ShopCart({ cartItems, setCartItems }) {
         <div>
             <h2>Your Shopping Cart</h2>
             <ul>
-                {cartItems.map(item => (
-                    <li key={item.id}>
-                        {item.title} - Quantity: {item.quantity} - Price: ${item.price * item.quantity}
-                        {item.title} - Quantity: {item.quantity} - Price: ${item.price * item.quantity}
-                        <button onClick={() => handleIncrementQuantity(item.id)}>+</button>
-                        <button onClick={() => handleDecrementQuantity(item.id)}>-</button>
-                        <button onClick={() => handleRemoveItem(item.id)}>Remove</button>
-                    </li>
-                ))}
+            {cartItems.map(item => (
+    <li key={item.id}>
+        {item.title} - Quantity:
+        <input
+            type="number"
+            value={item.quantity}
+            onChange={(e) => handleEditQuantity(item.id, parseInt(e.target.value))}
+        />
+        - Price: ${item.price * item.quantity}
+        <button onClick={() => handleIncrementQuantity(item.id)}>+</button>
+        <button onClick={() => handleDecrementQuantity(item.id)}>-</button>
+        <button onClick={() => handleRemoveItem(item.id)}>Remove</button>
+    </li>
+))}
+
             </ul>
             <p>Total Price: ${totalPrice}</p>
         </div>
